1a896cb45df6f92fc289aa8e728c5711b896ebe2
Matthew Muscat <matthew@mamis.com.au>
2017-06-08 15:16:01 +1000

    QA of SC-38
    - remove instances of object manager
    - add escaping to delivery comments in admin
    
    Signed-off-by: Matthew Muscat <matthew@mamis.com.au>

--- Observer/AddHtmlToOrderShippingViewObserver.php
+++ Observer/AddHtmlToOrderShippingViewObserver.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * Shippit Pty Ltd
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the terms
+ * that is available through the world-wide-web at this URL:
+ * http://www.shippit.com/terms
+ *
+ * @category   Shippit
+ * @copyright  Copyright (c) 2016 by Shippit Pty Ltd (http://www.shippit.com)
+ * @author     Matthew Muscat <matthew@mamis.com.au>
+ * @license    http://www.shippit.com/terms
+ */
+
 namespace Shippit\Shipping\Observer;
 
 use Magento\Framework\Event\Observer as EventObserver;
@@ -7,28 +22,48 @@ use Magento\Framework\Event\ObserverInterface;
 class AddHtmlToOrderShippingViewObserver implements ObserverInterface
 {
     /**
-     * @var \Magento\Framework\ObjectManagerInterface
+     * @var \Shippit\Shipping\Helper\Checkout
      */
-    protected $objectManager;
+    protected $helper;
 
     /**
-     * @param \Magento\Framework\ObjectManagerInterface $objectManager
+     * @var \Magento\Framework\View\Element\TemplateFactory
      */
-    public function __construct(\Magento\Framework\ObjectManagerInterface $objectManager)
-    {
-        $this->objectManager = $objectManager;
+    protected $templateFactory;
+
+    /**
+     * @param \Shippit\Shipping\Helper\Checkout                 $helper
+     * @param \Magento\Framework\View\Element\TemplateFactory   $templateFactory
+     */
+    public function __construct(
+        \Shippit\Shipping\Helper\Checkout $helper,
+        \Magento\Framework\View\Element\TemplateFactory $templateFactory
+    ) {
+        $this->helper = $helper;
+        $this->templateFactory = $templateFactory;
     }
 
     public function execute(EventObserver $observer)
     {
-        if ($observer->getElementName() == 'order_shipping_view') {
-            $orderShippingViewBlock = $observer->getLayout()->getBlock($observer->getElementName());
+        if ($observer->getElementName() == 'order_shipping_view'
+            && ($this->helper->isAuthorityToLeaveActive() || $this->helper->isDeliveryInstructionsActive())
+            ) {
+            $orderShippingViewBlock = $observer->getLayout()
+                ->getBlock($observer->getElementName());
+
             $order = $orderShippingViewBlock->getOrder();
-            $orderOptionsBlock = $this->objectManager->create('Magento\Framework\View\Element\Template');
-            $orderOptionsBlock->setShippitDeliveryInstructions($order->getShippitDeliveryInstructions());
-            $orderOptionsBlock->setShippitAuthorityToLeave($order->getShippitAuthorityToLeave());
-            $orderOptionsBlock->setTemplate('Shippit_Shipping::order/view/shippit_order_options.phtml');
-            $html = $observer->getTransport()->getOutput() . $orderOptionsBlock->toHtml();
+
+            $shippitOrderOptions = $this->templateFactory->create();
+
+            // Set the data for the block
+            $shippitOrderOptions->setHelper($this->helper)
+                ->setShippitDeliveryInstructions($order->getShippitDeliveryInstructions())
+                ->setShippitAuthorityToLeave($order->getShippitAuthorityToLeave());
+
+            $shippitOrderOptions->setTemplate('Shippit_Shipping::order/view/shippit_order_options.phtml');
+
+            // Append to the blocks output and set
+            $html = $observer->getTransport()->getOutput() . $shippitOrderOptions->toHtml();
             $observer->getTransport()->setOutput($html);
         }
     }
